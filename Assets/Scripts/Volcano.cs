4c35ad2849244f6afc2b7889f52f9c83cb3f315c
PC Portable Hugo <hugo.raguin@gmail.com>
2019-10-16 23:04:27 +0200

    Yes

---
+++ Assets/Scripts/Volcano.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Volcano : Props
+{
+    public float bouncePower;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            PlayerController pController = collision.transform.GetComponent<PlayerController>();
+            if (pController == null) return;
+            float speed = pController.GetSpeed();
+            speed *= bouncePower;
+            pController.SetSpeed(speed);
+        }
+    }
+}
